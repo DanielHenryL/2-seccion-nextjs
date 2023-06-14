cb869705b345f62ecc94206b59c657134561ceb4
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-14 15:14:11 -0500

    instalando nextui e implementando el tema dark

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
+'use client';
+import { Button } from '@nextui-org/react'
 import { Inter } from 'next/font/google'
-
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <Button color={'gradient'}>Hola mundo</Button>
     </>
   )
 }
