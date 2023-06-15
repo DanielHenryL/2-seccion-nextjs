f400ff06020053b78711130fa5e424024ee1fdc6
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-14 20:29:13 -0500

    getStaticProps y cargar los pokemones de forma estatica

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,43 @@
 'use client';
+import { pokeApi } from '@/api';
 import { Layout } from '@/components/layouts';
-import { Button } from '@nextui-org/react'
+import { GetStaticProps } from 'next';
 import { Inter } from 'next/font/google'
 
-// const inter = Inter({ subsets: ['latin'] })
 
-export default function HomePage() {
+// import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+// type Repo = {
+//   name: string
+//   stargazers_count: number
+// }
+ 
+
+export default function HomePage(props) {
+  console.log({props})
   return (
     <Layout title={'Listado de pokemon'}>
-      <Button color={'gradient'}>Hola mundo</Button>
+      <ul>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+        <li>Pokémon</li>
+       
+      </ul>
     </Layout>
   )
 }
+export const getStaticProps: GetStaticProps = async (ctx) => {
+    const {data} = await pokeApi.get('/pokemon?limit=151')
+    
+    return {
+      props: { 
+        pokemons:data.results
+      } 
+    }
+  }
