50c1b80fc23054c024093834710af6cbb9003d35
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-17 14:52:45 -0500

    Open Graph Meta Tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -6,14 +6,18 @@ interface Props{
   children:JSX.Element,
   title?:string
 }
+const origin = ( typeof window === 'undefined') ? '':window.location.origin
 export const Layout:FC<PropsWithChildren <Props>> = ({children, title}) => {
   return (
     <>
       <Head>
           <title>{title || 'PokemonApp'}</title>
-          <meta name='author' content='Fernando Herrera' />
-          <meta name='descripcion' content='Informacion de un pokemon XXXXXX' />
+          <meta name='author' content='Henry Daniel' />
+          <meta name='descripcion' content={`Informacion de un pokemon ${title}`} />
           <meta name='keywords' content={`${title}, pokemon, pokedex `} />
+          <meta property="og:title" content={`Informacion de un pokemon ${title}`} />
+          <meta property="og:description" content={`Esta es la pagina sobre ${title}`} />
+          <meta property="og:image" content={`${origin}/imgs/banner.png`} />
       </Head>
       <Navbar />
 
